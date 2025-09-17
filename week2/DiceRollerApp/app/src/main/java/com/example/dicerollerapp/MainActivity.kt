ff67aeda142d7c41980bc31ed390e86039510050
NguyenVanHung2004 <22028118@vnu.edu.vn>
2025-09-17 18:35:01 +0700

    Week2: Dice Roller App

---
+++ week2/DiceRollerApp/app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.dicerollerapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val buttonRoll = findViewById<Button>(R.id.btnRoll);
+        val diceImage = findViewById<ImageView>(R.id.ivDice)
+        buttonRoll.setOnClickListener {
+            val value = Random.nextInt(1,7)
+
+           val imageSource = when(value) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+            diceImage.setImageResource(imageSource)
+        }
+
+
+
+    }
+}
