9c31ca34bbd62ae9612a1f52e220314a652c9688
NguyenVanHung2004 <22028118@vnu.edu.vn>
2025-10-19 22:29:07 +0700

    Bus Schedule finish all problems

--- Unit6/Pathway2/Bus Schedule/basic-android-kotlin-compose-training-bus-schedule-app/app/src/main/java/com/example/busschedule/ui/BusScheduleScreens.kt
+++ Unit6/Pathway2/Bus Schedule/basic-android-kotlin-compose-training-bus-schedule-app/app/src/main/java/com/example/busschedule/ui/BusScheduleScreens.kt
@@ -58,6 +58,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.busschedule.ui.BusScheduleViewModel
 import com.example.busschedule.R
 import com.example.busschedule.data.BusSchedule
 import com.example.busschedule.ui.theme.BusScheduleTheme
